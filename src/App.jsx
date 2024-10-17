1c72577e6fa50538e0e2392d1e10e10ce73baaa0
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-10-17 09:54:45 +0330

    react-query and devtools for that installed and general configs implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Router from "router/Router";
+import defaultOptions from "configs/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions,
+  });
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
       <Toaster position="top-left" />
-    </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
